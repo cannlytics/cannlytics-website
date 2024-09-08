2b687708962999be2eeedab49b1455d83cc42977
Keegan Skeate <keeganskeate@gmail.com>
2024-09-07 22:07:24 -0700

    Folded daily report into `admin_update` function

--- functions/admin_update/main.py
+++ functions/admin_update/main.py
@@ -4,11 +4,12 @@ Copyright (c) 2024 Cannlytics
 
 Authors: Keegan Skeate <https://github.com/keeganskeate>
 Created: 2/10/2024
-Updated: 2/10/2024
+Updated: 9/7/2024
 License: MIT License <https://github.com/cannlytics/cannlytics-website/blob/main/LICENSE>
 """
 # Internal imports.
 import base64
+from datetime import datetime
 import os
 import requests
 
@@ -17,7 +18,60 @@ from cannlytics.firebase import (
     get_collection,
     update_documents,
 )
-from firebase_admin import initialize_app, firestore
+from firebase_admin import auth, initialize_app, firestore
+
+
+def update_user_stats():
+    """Update user statistics."""
+
+    # Initialize Firestore.
+    try:
+        initialize_app()
+    except ValueError:
+        pass
+
+    # Get user statistics.
+    users = []
+    monthly_users = 0
+    start_of_month = datetime(datetime.now().year, datetime.now().month, 1)
+    print('Getting user statistics...')
+    page = auth.list_users()
+    while page:
+        for user in page.users:
+            users.append(user)
+            timestamp = user.user_metadata.creation_timestamp
+            created_at = datetime.fromtimestamp(timestamp / 1000)
+            if created_at >= start_of_month:
+                monthly_users += 1
+        page = page.get_next_page()
+    number_of_users = len(users)
+    print(f'Total number of users: {number_of_users}')
+    print(f'Number of new users this month: {monthly_users}')
+
+    # Get current user stats and see if there were any change in users.
+    db = firestore.client()
+    ref = db.collection('stats').document('users')
+    current_user_stats = ref.get().to_dict()
+    if current_user_stats is None:
+        current_user_stats = {
+            'total_users': 0,
+            'monthly_users': 0,
+        }
+
+    # Calculate the daily change in users.
+    updated_user_stats = {
+        'total_users': number_of_users,
+        'monthly_users': monthly_users,
+    }
+    updated_user_stats['change_in_users'] = updated_user_stats['total_users'] - current_user_stats['total_users']
+    updated_user_stats['change_in_monthly_users'] = updated_user_stats['monthly_users'] - current_user_stats['monthly_users']
+
+    # Update user stats if they changed.
+    ref.set(updated_user_stats)
+    print('Updated stats/users document.')
+
+    # Return the updated user stats.
+    return updated_user_stats
 
 
 def admin_update(event, context):
@@ -40,10 +94,8 @@ def admin_update(event, context):
         pass
     database = firestore.client()
 
-    # TODO: Find any new users.
-
-
-    # TODO: Find any new organizations.
+    # Update user statistics.
+    user_stats = update_user_stats()
 
 
     # TODO: Get the recent logs.
@@ -75,3 +127,25 @@ def admin_update(event, context):
 
 
     # Optional: Any new research articles about cannabis.
+
+    # TODO: Get counts of data types.
+
+
+    # Optional: Get counts of logs.
+
+
+    # TODO: Read report template.
+
+
+    # TODO: Fill-in report template.
+
+
+    # TODO: Generate the report.
+
+
+# === Tests ===
+# [✓] Tested: 2024-09-07 by Keegan Skeate <keegan@cannlytics>
+if __name__ == '__main__':
+
+    # Update user statistics.
+    user_stats = update_user_stats()
